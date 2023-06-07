3199ea7e36f10825938483c35e803744c96517aa
ShioN <shion@arch4e.com>
2023-06-08 07:11:52 +0900

    update: add vim plugin

--- vimplugins.sh
+++ vimplugins.sh
@@ -4,4 +4,5 @@ git clone https://github.com/arcticicestudio/nord-vim.git $HOME/.vim/pack/vpm/op
 mkdir -p ~/.vim/pack/vpm/start
 git clone https://github.com/itchyny/lightline.vim.git $HOME/.vim/pack/vpm/start/lightline.vim
 git clone https://github.com/preservim/nerdtree.git $HOME/.vim/pack/vpm/start/nerdtree
+git clone https://github.com/airblade/vim-gitgutter.git $HOME/.vim/pack/vpm/start/vim-gitgutter
 
